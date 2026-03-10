b55966f4eb5f81f47a759f4130d65bf389fcfe26
Amanek1011 <kkat51866@gmail.com>
2026-03-10 18:56:02 +0600

    Models

--- apps/shveya/admin.py
+++ apps/shveya/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Party, Material
 
-# Register your models here.
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ("id", "batch_number", "design", "created_at")
+    search_fields = ("batch_number", "design")
+    ordering = ("-created_at",)
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "party",
+        "color",
+        "quantity_line",
+        "tshirt_count",
+        "four_x_count",
+        "raspash_count",
+        "beika_count",
+        "strochka_count",
+        "gorlo_count",
+        "ytyg_count",
+        "otk_count",
+        "ypakovka_count",
+    )
+
+    list_display_links = ("id", "party", "color")
+
+    list_filter = (
+        "party",
+        "color",
+    )
+
+    search_fields = (
+        "party__batch_number",
+        "color",
+    )
+
+    ordering = ("-id",)
+
--- apps/shveya/models.py
+++ apps/shveya/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class Party(models.Model):
+    batch_number = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    design = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Batch {self.batch_number} - {self.design}"
+
+class Material(models.Model):
+    party = models.ForeignKey(
+        Party,
+        on_delete=models.CASCADE,
+        related_name="materials"
+    )
+
+    color = models.CharField(max_length=50)
+    quantity_line = models.IntegerField()
+    tshirt_count = models.IntegerField()
+
+    four_x = models.CharField(max_length=50, blank=True)
+    four_x_count = models.IntegerField(null=True, blank=True)
+
+    raspash = models.CharField(max_length=50, blank=True)
+    raspash_count = models.IntegerField(null=True, blank=True)
+
+    beika = models.CharField(max_length=50, blank=True)
+    beika_count = models.IntegerField(null=True, blank=True)
+
+    strochka =  models.CharField(max_length=50, blank=True)
+    strochka_count = models.IntegerField(null=True, blank=True)
+
+    gorlo = models.CharField(max_length=50, blank=True)
+    gorlo_count = models.IntegerField(null=True, blank=True)
+
+    ytyg = models.CharField(max_length=50, blank=True)
+    ytyg_count = models.IntegerField(null=True, blank=True)
+
+    otk = models.CharField(max_length=50, blank=True)
+    otk_count = models.IntegerField(null=True, blank=True)
+
+    ypakovka = models.CharField(max_length=50, blank=True)
+    ypakovka_count = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.party.batch_number} - {self.color}"
